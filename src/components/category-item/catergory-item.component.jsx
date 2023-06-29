162386dc143d1c30d6174d10125b9e916687dded
prat <pratikkatti4@gmial.com>
2023-06-29 20:47:08 +0530

    first commit

---
+++ src/components/category-item/catergory-item.component.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+import './catergory-item.scss'
+
+export default function CatergoryItem({category}) {
+  const {imageUrl,title}=category;
+  return (
+    <div className="category-container" >
+          
+          <div className="background-image" style={{
+            backgroundImage:`url(${imageUrl})`
+          }}/>
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop now</p>
+          </div>
+        </div>
+  )
+}
